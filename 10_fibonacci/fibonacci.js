97616c4333b63a9bcc314d50cfd6bd5cef734b6b
Ari <afriedgut1@gmail.com>
2022-10-08 15:16:09 +1100

    fibonacci challenge

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+  if (num < 0) return "OOPS";
+  let fibArr = [1];
+  let fibNum = 1;
+  for (let i = 0; i < num; i++) {
+    if (i === 0) {
+      fibArr.push(fibNum);
+      continue;
+    }
+    fibNum = fibArr[i-1] + fibArr[i];
+    fibArr.push(fibNum);
+  }
+  return fibArr[num - 1];
 };
 
 // Do not edit below this line
