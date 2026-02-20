db60f7530d5b4db0d885e0803eda251fcd3aeaf2
Ari <afriedgut1@gmail.com>
2026-02-20 15:43:20 +1100

    pushing changes from a while back

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,6 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+  let newStr = str.toLowerCase().replace(/[^a-z]/g, "");
+  return newStr.split("").reverse().join("") === newStr;
 };
 
 // Do not edit below this line
--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,11 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+  let currentYear = new Date().getFullYear();
+  const sortedByAge = people.sort((personA, personB) => {
+   let firstAge = (personA.yearOfDeath || currentYear) - personA.yearOfBirth;
+   let secondAge = (personB.yearOfDeath || currentYear) - personB.yearOfBirth;
+   return firstAge > secondAge ? -1 : 1;
+  });
+  return sortedByAge[0];
 };
 
 // Do not edit below this line
